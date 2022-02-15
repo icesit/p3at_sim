4d958ec94020bb2a714084f23cf596473d710729
XueWuyang <1259918460@qq.com>
2022-02-15 16:27:44 +0800

    get the tf back.

--- gazebo/src/realposenotf.py
+++ gazebo/src/realposenotf.py
@@ -8,10 +8,15 @@ import math
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 from nav_msgs.msg import Odometry
 
+t_prior_visited = [ 17.5, 2.5, 0.0]
+
 def realodomCB(data):
     global posecovarpub, posepub
     posecov_msg = PoseWithCovarianceStamped()
     pose_msg = PoseStamped()
+    data.pose.pose.position.x = data.pose.pose.position.x - t_prior_visited[0]
+    data.pose.pose.position.y = data.pose.pose.position.y - t_prior_visited[1]
+    data.pose.pose.position.z = data.pose.pose.position.z - t_prior_visited[2]
     posecov_msg.pose = data.pose
     pose_msg.pose = data.pose.pose
     posecov_msg.header = data.header
